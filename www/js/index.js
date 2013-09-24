5794fb4cb07545642cb574b4c63593ee2430a006
kunleoyafajo <kunleoyafajo@yahoo.com>
2013-09-24 00:15:13 -0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -45,7 +45,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
         
         //Redirect app web page to your REDCap URL
-        window.location.href='https://hprfstudies.org/redcap';
+        window.location.href='http://mobile.activepair.net';
         console.log('Received Event: ' + id);
     }
 };
